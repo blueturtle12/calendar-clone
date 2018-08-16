16bddc15023583cfe1d66241446990efd0610620
hachem <blueturtle1293@gmail.com>
2018-08-16 14:59:00 +0300

    styled modal

--- src/js/calendarGrid.js
+++ src/js/calendarGrid.js
@@ -5,9 +5,14 @@ const calendarGrid = () => {
                 ['12','13','14','15','16','17','18'],
                 ['19','20','21','22','23','24','25'],
                 ['26','27','28','29','30','31','sep1']];
-
-    function bima () {
-        console.log('test');
+                
+    const closeModal = (modal) => {
+        modal.classList.remove("modal-window");
+        modal.classList.add("modal-window--closed");
+    }
+    const openModal = (modal) => {
+        modal.classList.remove("modal-window--closed");
+        modal.classList.add("modal-window");
     }
 
     for(let j = 0 ; j < 5 ; j++) {
@@ -16,43 +21,37 @@ const calendarGrid = () => {
         for( let i = 0; i< 7 ; i++) {
             let createBox = document.createElement('div');
             createBox.className = "grid__presentation__hour";
-            //createDiv.innerHTML = "test";
             createBox.innerHTML = "<h5 class='presentation__hour__dates'>"+dates[j][i]+"</h5>"
-            let bimka = document.createElement('a');
-            bimka.className = "day__event";
-            bimka.innerHTML = "open modal";
+            let dailyEvent = document.createElement('a');
+            dailyEvent.className = "day__event";
+            dailyEvent.innerHTML = "open modal";
+            let modalContainer = document.querySelector("#modal");
+            
             if( (i === 1 && j === 0) || (i === 5 && j === 4) || (i === 3 && j === 2) ) {
-                createBox.appendChild(bimka);
-                let modalContainer = document.createElement('div');
-                modalContainer.className = "modal-window";
-                modalContainer.innerHTML = "<div><a href='#modal-close' title='Close' class='modal-close'>close &times;</a>"+
-                "<h1>testewi</h1><div>Teeestewi</div></div>"
-                bimka.addEventListener("click", (e) => {
-                createBox.appendChild(modalContainer);
-            });
+
+                createBox.appendChild(dailyEvent);
+
+                dailyEvent.addEventListener("click", () => {
+                    createBox.appendChild(modalContainer);
+                    openModal(modalContainer);
+                });
+                window.addEventListener('click', (e)=> {
+                    if(e.target == modalContainer) {
+                        closeModal(modalContainer);
+                    }
+                });
+
+                let modalCloseButton = document.querySelector('.modal-close'); 
+                modalCloseButton.addEventListener("click", () => {
+                    closeModal(modalContainer);
+                });
+
             }
             
-            /*
-            .onclick = function () {
-                console.log(testewi);
-            }
-            */
-            /**/
-           /* createBox.addEventListener('hover', () => {
-                console.log('testewi');
-            }) */
             createDiv.appendChild(createBox);
         }
         hoursGrid.appendChild(createDiv);
-    }
-    
-    
-        /*
-        createBox.innherHtml = "<div id='open-modal' class='modal-window'><div><a title='Close' class='modal-close'>close &times;</a><h1>CSS Modal</h1><div>The quick brown fox jumped over the lazy dog.</div></div>"
-        */
-       //console.log('testewi');
-    
-    
+    }   
 }
 
 
